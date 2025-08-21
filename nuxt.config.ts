b16f94889333812906d56caeb1a32a8c4282fa53
fridolph <249121486@qq.com>
2025-08-22 01:23:27 +0800

    feat: :sparkles: Nuxt 初始化项目 ，nuxt-ui，全新的尝试

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  css: ['~/assets/css/main.css'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui'
+  ]
+})
